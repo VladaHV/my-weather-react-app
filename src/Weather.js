4ea7422adfaac23c6f5c7c7cd33179a5224322f9
VladaHV <110497335+VladaHV@users.noreply.github.com>
2023-09-19 11:21:18 +0300

    Create new Weather project, add bootstrap

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>How are you?
+      <footer>
+        This site coded by{" "}
+        <a
+          href="https://gregarious-figolla-9cbc1d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Vlada Honchar{" "}
+        </a>
+        and is{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
