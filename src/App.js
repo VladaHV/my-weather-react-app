28f09852c57fca0b1ec4613146d1849598595c50
VladaHV <110497335+VladaHV@users.noreply.github.com>
2023-09-19 17:23:56 +0300

    add basic styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          This site coded by{" "}
+          <a
+            href="https://gregarious-figolla-9cbc1d.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vlada Honchar{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/VladaHV/my-weather-react-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,29 +4,48 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-6">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
       <h1>Kryvyi Rih</h1>
       <ul>
         <li>Tuesday 13:00</li>
         <li>Clear Sky</li>
       </ul>
-      <footer>
-        This site coded by{" "}
-        <a
-          href="https://gregarious-figolla-9cbc1d.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Vlada Honchar{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/VladaHV/my-weather-react-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            className="float-start"
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="clear sky"
+          />
+          <span className="temp">6</span>
+          <span className="unit">ºC</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Presipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 30 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
